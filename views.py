41f31568c0639971d8dc79f99b6c55e4cf1002ab
SnehaRaj-1234 <87054069+SnehaRaj-1234@users.noreply.github.com>
2024-09-04 00:21:03 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render,HttpResponse, redirect
 from django.contrib import messages
-from .models import Registers
+from .models import feedbform,futuresupport,userdonate,feedbackforms
 from django.contrib.auth.models import User,auth
 from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 def open(request):
     return render(request,'index.html')
-
-
 
 #register
 def register(request):
@@ -106,12 +106,74 @@ def adminpg(request):
 
 @login_required
 def userdonate(request):
-    return render(request,'userdonate.html')
+    if request.method=="POST":
+        books=request.POST["books"]
+        stationery_items=request.POST["stationery_items"]
+        clothes=request.POST["clothes"]
+        others=request.POST["others"]
+        address=request.POST["address"]
+        date =request.POST["date"]
+        time =request.POST["time"]
+        instance=userdonate.objects.create(books=books,stationery_items=stationery_items,clothes=clothes,others=others,address=address,date=date,time=time)
+       
+        instance.save()
+        messages.info(request,'Submitted successfully!')
+        return render(request,'userdonate.html')
+    return render(request,'userdonate.html',{'instance': instance})
+
+
 def donatedItems(request):
-    return render(request,'donatedItems.html')
-
-
+    data=userdonate.objects.all()
+    return render(request,'donatedItems.html',{'data': data})
 
 
 def mission(request):
     return render(request,'mission.html')
+
+#admin_donorlist
+def admdonarlist(request):
+    return render(request,'admdonarlist.html')
+
+#feedbackform user
+def ufeedbackform(request):
+
+    if request.method=="POST":
+
+        rating=request.POST["rating"]
+        experience=request.POST["experience"]
+        suggestions=request.POST["suggestions"]
+        positive_aspects=request.POST["positive-aspects"]
+        areas_for_improvement=request.POST["areas-for-improvement"]
+        instance=feedbackforms.objects.create(rating=rating,experience=experience,suggestions=suggestions,positive_aspects=positive_aspects,areas_for_improvement=areas_for_improvement)
+       
+        instance.save()
+        messages.info(request,'Feedback submitted successfully!')
+        return render(request,'ufeedbackform.html')
+
+    return render(request,'ufeedbackform.html')
+
+
+#admin_feedbackform_viewing
+def admfeedbform(request):
+    feedbacks=feedbackforms.objects.all()
+    # context = feedbacks
+    # print(feedbacks)
+    return render(request,'admfeedbform.html', {'feedbacks': feedbacks})
+
+def userfsupport(request):
+    if request.method=="POST":
+
+        name=request.POST["name"]
+        contact=request.POST["contact"]
+        address=request.POST["address"]
+        instance=futuresupport.objects.create(name=name,contact=contact,address=address)
+        instance.save()
+        messages.info(request,' submitted successfully!')
+        return redirect('userfsupport')
+    return render(request,'userfsupport.html')
+
+
+#admin_futuresupport page
+def admfsupport(request):
+    fsupport=futuresupport.objects.all()
+    return render(request,'admfsupport.html', {'fsupport': fsupport})
