314299358d5dad578b3a25ae7c3b1e659cd3eef5
SnehaRaj-1234 <87054069+SnehaRaj-1234@users.noreply.github.com>
2024-10-20 22:32:35 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import feedbform,futuresupport,userdonate,feedbackforms
+from .models import futuresupport,userdonation,feedbackforms,donation
 
-admin.site.register(feedbform) #table registering
-admin.site.register(futuresupport) #table registering
-admin.site.register(userdonate) #table registering
-admin.site.register(feedbackforms) #table registering
+class feedbackformAdmin(admin.ModelAdmin):
+    list_display=['rating','suggestions','areas_for_improvement']   
+admin.site.register(feedbackforms,feedbackformAdmin)
+
+
+class futuresupportAdmin(admin.ModelAdmin):
+    list_display=['name','contact','address']   
+admin.site.register(futuresupport,futuresupportAdmin)
+
+
+class userdonationAdmin(admin.ModelAdmin):
+    list_display=['name','d_item','date','time','collected']   
+admin.site.register(userdonation,userdonationAdmin)
+
+
+class userfdonationAdmin(admin.ModelAdmin):
+    list_display=['name','donor_id','amount']   
+admin.site.register(donation,userfdonationAdmin)
