d35999b3a6f0a238d5415c3ee274a6b0c2de832f
SnehaRaj-1234 <87054069+SnehaRaj-1234@users.noreply.github.com>
2024-10-20 22:31:12 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,33 +1,21 @@
 from django.db import models
-
-# Create your models here.
-class feedbform(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    rating=models.CharField(max_length=50)
-    experience=models.CharField(max_length=200)
-    suggestions=models.CharField(max_length=300)
-    positive_aspects=models.CharField(max_length=300)
-    areas_for_improvement=models.CharField(max_length=300)
+from django.contrib.auth.models import User
     
 class futuresupport(models.Model):
     name=models.CharField(max_length=100)
     contact=models.CharField(max_length=12)
     address=models.TextField()
     
-class userdonate(models.Model):
-    DONATION_CHOICES = [
-        ('Books', 'Books'),
-        ('Stationery Items', 'Stationery Items'),
-        ('Clothes', 'Clothes'),
-        ('Others', 'Others'),
-    ]
-    name=models.CharField(max_length=50)
-    d_items=models.TextField()
+
+class userdonation(models.Model):
+    name=models.TextField()
+    d_item=models.TextField()
     address=models.TextField()
     date=models.DateField()
     time=models.TimeField()
-    #  def __str__(self):
-    #     return f"{self.donation_type} - {self.address}"
+    user=models.CharField(max_length=100,blank=False,null=True)
+    collected=models.BooleanField(default=False)
+
 
 class feedbackforms(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -36,5 +24,10 @@ class feedbackforms(models.Model):
     suggestions=models.CharField(max_length=300)
     positive_aspects=models.CharField(max_length=300)
     areas_for_improvement=models.CharField(max_length=300)
-        
-        
+
+class donation(models.Model):
+    
+    name=models.CharField(max_length=50,null=False)
+    donor_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    amount=models.DecimalField(max_digits=5,decimal_places=2)
+            
